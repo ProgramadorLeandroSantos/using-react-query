77368a89795bb0dd32b1a64040730c1796912a40
ProgramadorLeandroSantos <programadorleandrosantos@gmail.com>
2023-04-22 21:10:43 -0300

    adding loading on list

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,34 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     FlatList,
     Text,
     TouchableOpacity,
     View
 } from 'react-native';
+import { api } from '../../api';
 
 
 function List(): JSX.Element {
 
     const [listData, setListData] = useState([]);
+    const [isDataLoading, setIsDataLoading] = useState(true);
+
+    const fetchListData = async () => {
+        setIsDataLoading(true);
+        const data = await api.getNames();
+        setListData(data)
+        setIsDataLoading(false);
+    }
+
+    useEffect(() => {
+        fetchListData()
+    }, []);
 
 
     return (
-        <FlatList
-            contentContainerStyle={{ flex: 2, marginVertical: 10, alignItems:'center' }}
-            renderItem={({ item }) => {
-                return (
-                    <View style={{justifyContent:'space-between', alignItems:'center', flexDirection:'row', width:300}}>
-                        <Text style={{ fontSize: 20, textAlign: 'center' }}>{item}</Text>
-                        <TouchableOpacity
-                            onPress={() => console.log(item)}
-                        >
-                            <Text style={{ fontSize: 22, fontWeight: 'bold' }}>x</Text>
-                        </TouchableOpacity>
-                    </View>
-                )
-            }}
-            data={listData}
-        />
+        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 2 }}>
+            {isDataLoading ? (<Text style={{ fontSize: 32, color: 'black' }}>Loading...</Text>) : (
+                <FlatList
+                    contentContainerStyle={{ flex: 2, marginVertical: 10, alignItems: 'center' }}
+                    renderItem={({ item }) => {
+                        return (
+                            <View style={{ justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row', width: 300 }}>
+                                <Text style={{ fontSize: 20, textAlign: 'center' }}>{item.name}</Text>
+                                <TouchableOpacity
+                                    onPress={() => console.log(item)}
+                                >
+                                    <Text style={{ fontSize: 22, fontWeight: 'bold' }}>x</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )
+                    }}
+                    data={listData}
+                />
+            )}
+
+        </View>
     );
 }
 export default List;
