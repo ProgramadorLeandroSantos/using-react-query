b88a597bdb80f683a48d76b3540f04b127ff0b72
ProgramadorLeandroSantos <programadorleandrosantos@gmail.com>
2023-04-22 21:31:23 -0300

    adding react query provider to app component

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,30 @@ import {
 } from 'react-native';
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { List,Bottom } from './components';
+import { List, Bottom } from './components';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+
+  const queryClient = new QueryClient();
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   return (
+    <QueryClientProvider client={queryClient}>
       <SafeAreaView style={{ flex: 1 }}>
         <StatusBar
           barStyle={isDarkMode ? 'light-content' : 'dark-content'}
           backgroundColor={backgroundStyle.backgroundColor}
         />
         <List />
-        <Bottom/>
+        <Bottom />
       </SafeAreaView>
+    </QueryClientProvider>
+
   );
 }
 export default App;
